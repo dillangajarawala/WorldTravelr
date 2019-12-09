5a96c7b5d2b8766f13bf71f2801ae276db7d17b3
Dillan Gajarawala <dillan.gajarawala@gmail.com>
2019-12-09 15:13:59 +0000

    favorites toggle implemented for each resort page

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,12 @@ $(document).ready(function() {
     $("#tabs").tabs();
 });
 
-$(document).ready(function() {
+function setData() {
     var page = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
     $.getJSON('resorts.json', function(data) {
         for (var i in data.resorts) {
             if (data.resorts[i].url === page) {
+                $('.favorites-toggle').attr('id', data.resorts[i].id);
                 $('.intro-content h1').text(data.resorts[i].name);
                 $('head title').text(data.resorts[i].name);
                 $('.intro-content .fill').attr('src', data.resorts[i].picture);
@@ -33,4 +34,42 @@ $(document).ready(function() {
             }
         }
     });
+};
+
+$(document).ready(function() {
+    var favResorts = JSON.parse(localStorage.getItem("favResorts"));
+    if (favResorts == null) {
+        favResorts = []
+    }
+    var resortId = $('.favorites-toggle').attr('id');
+    if (favResorts.includes(resortId)) {
+        $('.favorites-toggle').text('Delete From Favorites');
+        $('.favorites-toggle').css('background-color', 'red');
+        $('.favorites-toggle').attr('data-add', 'false');
+    } else {
+        $('.favorites-toggle').text('Add To Favorites');
+        $('.favorites-toggle').css('background-color', 'lime');
+        $('.favorites-toggle').attr('data-add', 'true');
+    }
 });
+
+$(document).ready(function() {
+    $('.favorites-toggle').on('click', function() {
+        $(this).attr('disabled', true);
+        var favResorts = JSON.parse(localStorage.getItem('favResorts'));
+        var toAdd = $(this).attr('data-add');
+        var resortId = $(this).attr('id');
+        if (toAdd === 'true') {
+            if (favResorts === null) {
+                favResorts = []
+            }
+            favResorts.push(resortId);
+            localStorage.setItem('favResorts', JSON.stringify(favResorts));
+        } else {
+            favResorts = favResorts.filter(function(item) {
+                return item !== resortId
+            });
+            localStorage.setItem('favResorts', JSON.stringify(favResorts));
+        }
+    })
+})
